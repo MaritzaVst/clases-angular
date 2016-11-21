f51ea6ce9a742d22aa4a3014f4c5e54d5ad44d1f
maritzavst <maritzasintitafur@gmail.com>
2016-11-20 19:09:34 -0500

    Tablas y listados dinámicas con ng-repeat

--- ejercicio-repeticiones/js/app.js
+++ ejercicio-repeticiones/js/app.js
@@ -5,6 +5,23 @@
 	app.controller('listadoCtrl', ['$scope', function($scope){
 		
 			$scope.listadoDeCompras = ["huevos", "carne", "arroz", "verduras", "condimentos"];
+			$scope.listadoProfesores = {
+				profesores: [{
+						nombre: "Lorena",
+						edad: 21,
+						clase: "CDE"
+					},
+					{
+						nombre: "Juan Carlos",
+						edad: 26,
+						clase: "TICS"
+					},
+					{
+						nombre: "Fernando",
+						edad: 29,
+						clase: "TEI"
+					}]
+			};
 
 	}]);
 
